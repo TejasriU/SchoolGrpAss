c5f6ca425f406c87e9645c86ef76c2f6e66bde48
uyyala tejasri <tejasriuyyala1010@gmail.com>
2021-06-03 18:08:42 +0530

    Add project files.

---
+++ StudMVC/Controllers/StudentMvcController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using StudMVC.Models;
+using System.Collections.Specialized;
+using System.Text;
+using System.Net;
+using System.Web.Http.Cors;
+
+namespace StudMVC.Controllers
+{
+    public class StudentMvcController : Controller
+    {
+        // GET: StudentMvc
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        public ActionResult Index()
+        {
+
+
+            IEnumerable<StudentView> empdata = null;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44304/api/";
+
+                var json = webClient.DownloadString("Students");
+                var list = JsonConvert.DeserializeObject<List<StudentView>>(json);
+                empdata = list.ToList();
+                return View(empdata);
+            }
+        }
+
+        // GET: StudentMvc/Details/5
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        public ActionResult Details(int id)
+        {
+
+            StudentView empdata;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44304/api/";
+
+                var json = webClient.DownloadString("Students/" + id);
+                //  var list = emp 
+                empdata = JsonConvert.DeserializeObject<StudentView>(json);
+            }
+            return View(empdata);
+        }
+
+        // GET: StudentMvc/Create
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentMvc/Create
+
+        [HttpPost]
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        public ActionResult Create(StudentView model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.BaseAddress = "https://localhost:44304/api/";
+                    var url = "Students/POST";
+                    //webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string data = JsonConvert.SerializeObject(model);
+                    var response = webClient.UploadString(url, data);
+                    JsonConvert.DeserializeObject<StudentView>(response);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: StudentMvc/Edit/5
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        public ActionResult Edit(int id)
+        {
+            StudentView empdata;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44304/api/";
+
+                var json = webClient.DownloadString("Students/" + id);
+                //  var list = emp 
+                empdata = JsonConvert.DeserializeObject<StudentView>(json);
+            }
+            return View(empdata);
+        }
+
+        // POST: StudentMvc/Edit/5
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        [HttpPost]
+        public ActionResult Edit(int id, StudentView model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.BaseAddress = "https://localhost:44304/api/Students/"+id;
+                    //var url = "Values/Put/" + id;
+                    //webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string data = JsonConvert.SerializeObject(model);
+
+                    var response = webClient.UploadString(webClient.BaseAddress, data);
+
+                    StudentView modeldata = JsonConvert.DeserializeObject<StudentView>(response);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: StudentMvc/Delete/5
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        public ActionResult Delete(int id)
+        {
+            StudentView empdata;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44304/api/";
+
+                var json = webClient.DownloadString("Students/" + id);
+                //  var list = emp 
+                empdata = JsonConvert.DeserializeObject<StudentView>(json);
+            }
+            return View(empdata);
+        }
+
+        // POST: StudentMvc/Delete/5
+        [EnableCors(headers: "*", methods: "*", origins: "*")]
+        [HttpPost]
+        public ActionResult Delete(int id, StudentView model)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                using (WebClient webClient = new WebClient())
+                {
+                    NameValueCollection nv = new NameValueCollection();
+                    var url = "https://localhost:44304/api/Students/" + id;
+                    var response = Encoding.ASCII.GetString(webClient.UploadValues(url, "DELETE", nv));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StudMVC/Models/StudentView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudMVC.Models
+{
+    public class StudentView
+    {
+        public int rollno { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string st_state { get; set; }
+        public string country { get; set; }
+    }
+}
